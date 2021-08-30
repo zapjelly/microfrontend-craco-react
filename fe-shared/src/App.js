f9961dc80b81d680f161e8b0eabb881a887b35e1
Cindy <cindy.pak@fifthdomain.com.au>
2021-08-30 10:39:39 +1000

    commiting before I add more

---
+++ fe-shared/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Button from './Button';
+
+function App() {
+  return (
+    <>
+      <h1>I am fe-shared</h1>
+      <div className="App">
+        <Button />
+      </div>
+    </>
+  );
+}
+
+export default App;
